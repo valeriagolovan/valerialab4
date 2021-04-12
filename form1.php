51f1f8b7af5a76e9d820e4cdb2f37254b1d5e5c8
Валерия Головань <lera.golovan.01@mail.ru>
2021-04-12 08:55:45 +0300

    Лабораторная 4

--- form1.php
+++ form1.php
@@ -16,20 +16,9 @@
     <div id="cont" class="container">
       <div id="form" class="col-12 order-lg-3 order-sm-2">
 
-        <?php
-          if (!empty($messages)) {
-            print('<div id="messages">');
-            // Выводим все сообщения.
-            foreach ($messages as $message) {
-              print($message);
-            }
-            print('</div>');
-          }
-
-          // Далее выводим форму отмечая элементы с ошибками классом error
-          // и задавая начальные значения элементов ранее сохраненными.
-        ?>
+       
         <form action="" method="POST">
+        <?php if ($errors['fio']) {print $messages[1];} ?>
           ФИО:
           <label>
             <input name="fio" placeholder="Введите ФИО" 
@@ -37,64 +26,66 @@
           </label>
           <br>
           <br>
+          <?php if ($errors['email']) {print $messages[2];} ?>
           E-mail:
           <label>
             <input type="email" name="email" placeholder="Введите e-mail" 
             <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>">
           </label>
+          <?php if ($errors['yob']) {print $messages[4];} ?>
           <p>Ваш год рождения:</p>
           <label>
           <input name="yob" placeholder="Введите год рождения" 
             <?php if ($errors['yob']) {print 'class="error"';} ?> value="<?php print $values['yob']; ?>">
           </label>
-
+          <?php if ($errors['radio-pol']) {print $messages[5];} ?>
           <p>Пол:</p>
           <label>
-            <input type="radio" name="radio-pol" checked="checked" value="M" checked = "checked">М
+            <input type="radio" name="radio-pol" value="M" <?php if($values['radio-pol']=='M') print "checked";?>>М
           </label>
           <label>
-            <input type="radio" name="radio-pol" value="W"/>Ж
+            <input type="radio" name="radio-pol" value="W" <?php if($values['radio-pol']=='W') print "checked";?>/>Ж
           </label>
-
+          <?php if ($errors['radio-kon']) {print $messages[6];} ?>
           <p>Количество конечностей</p><br />
           <label>
-            <input type="radio" name="radio-kon" value="0" />0
+            <input type="radio" name="radio-kon" value="0" <?php if($values['radio-kon']=='0') print "checked";?>/>0
           </label>
           <label>
-            <input type="radio" name="radio-kon" value="1" />1
+            <input type="radio" name="radio-kon" value="1" <?php if($values['radio-kon']=='1') print "checked";?>/>1
           </label>
           <label>
-            <input type="radio" name="radio-kon" value="2" />2
+            <input type="radio" name="radio-kon" value="2" <?php if($values['radio-kon']=='2') print "checked";?>/>2
           </label>
           <label>
-            <input type="radio" name="radio-kon" value="3" />3
+            <input type="radio" name="radio-kon" value="3" <?php if($values['radio-kon']=='3') print "checked";?>/>3
           </label>
           <label>
-            <input type="radio" checked="checked" name="radio-kon" value="4" />4
+            <input type="radio" name="radio-kon" value="4" <?php if($values['radio-kon']=='4') print "checked";?> />4
           </label>
           <label>
-            <input type="radio" name="radio-kon" value="5" />5
+            <input type="radio" name="radio-kon" value="5" <?php if($values['radio-kon']=='5') print "checked";?> />5
           </label>
-
+          <?php if ($errors['ability']) {print $messages[7];} ?>
           <p>Сверхспособности, если есть</p>
           <label>
-            <select name="sp-sp[]" multiple=multiple>
-              <option value="Immortal">Бессмертие</option>
-              <option value="Through the walls"> Прохождение сквозь стены</option>
-              <option value="Levitation">Левитация</option>
+            <select name="sp-sp[]" multiple=multiple <?php if ($errors['ability']) {print 'class="error"';} ?>>
+              <option value="0" <?php if(in_array("0", $ability)) print "selected";?>>Бессмертие</option>
+              <option value="1" <?php if(in_array("1", $ability)) print "selected";?>> Прохождение сквозь стены</option>
+              <option value="2" <?php if(in_array("2", $ability)) print "selected";?>>Левитация</option>
             </select>
           </label>
-
+          <?php if ($errors['biography']) {print $messages[3];} ?>
           <p id="bio">Биография</p>
           <label>
             <textarea placeholder="Напишите о себе" name="biography" rows="6" cols="60" 
             <?php if ($errors['biography']) {print 'class="error"';} ?>><?php print $values['biography'];?></textarea>
           </label>
           <br>
-
+          <?php if ($errors['contract']) {print $messages[8];} ?>
           <label>
             С контрактом ознакомлен
-            <input type="checkbox" name="check-ok" checked="checked">
+            <input type="checkbox" name="contract" <?php if ($errors['contract']) {print 'class="error"';} ?>>
           </label>
           <br>
           <input type="submit" value="Отправить">
--- index.php
+++ index.php
@@ -29,6 +29,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $errors['email'] = !empty($_COOKIE['email_error']);
   $errors['biography'] = !empty($_COOKIE['biography_error']);
   $errors['yob'] = !empty($_COOKIE['yob_error']);
+  $errors['radio-pol'] = !empty($_COOKIE['radio-pol_error']);
+  $errors['radio-kon'] = !empty($_COOKIE['radio-kon_error']);
+  $errors['ability'] = !empty($_COOKIE['ability_error']);
+  $errors['contract'] = !empty($_COOKIE['contract_error']);
+
 
 
   // Выдаем сообщения об ошибках.
@@ -56,6 +61,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // Выводим сообщение.
     $messages[4] = '<div class="error">Введите дату рождения</div>';
   }
+  if ($errors['radio-pol']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('radio-pol_error', '', 100000);
+    // Выводим сообщение.
+    $messages[5] = '<div class="error">Выберите пол/div>';
+  }
+  if ($errors['radio-kon']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('radio-kon_error', '', 100000);
+    // Выводим сообщение.
+    $messages[6] = '<div class="error">Выберите количество конечностей</div>';
+  }
+  if ($errors['ability']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('ability_error', '', 100000);
+    // Выводим сообщение.
+    $messages[7] = '<div class="error">Выберите супер способности</div>';
+  }
+  if ($errors['contract']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('contract_error', '', 100000);
+    // Выводим сообщение.
+    $messages[8] = '<div class="error">Необходимо согласие</div>';
+  }
 
   // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
@@ -63,6 +92,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
   $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
   $values['yob'] = empty($_COOKIE['yob_value']) ? '' : $_COOKIE['yob_value'];
+  $values['radio-pol'] = empty($_COOKIE['radio-pol_value']) ? '' : $_COOKIE['radio-pol_value'];
+  $values['radio-kon'] = empty($_COOKIE['radio-kon_value']) ? '' : $_COOKIE['radio-kon_value'];
+  $values['contract'] = empty($_COOKIE['contract_value']) ? '' : $_COOKIE['contract_value'];
+  // создаем массив способностей
+  $ability = array();
+  $ability = empty($_COOKIE['ability_values']) ? array() : unserialize($_COOKIE['ability_values'], ["allowed_classes" => false]);
+  // unserialize принимает одну сериализованную переменную и конвертирует её обратно в значение PHP.
+  // allowed_classes - Массив имён классов, которые должны быть приняты, false для указания не принимать никаких классов
 
 
   // Включаем содержимое файла form.php.
@@ -83,6 +120,7 @@ else {
     // Сохраняем ранее введенное в форму значение на год.
     setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60 * 365);
   }
+
   if (empty($_POST['email']) || preg_match("/@mail.ru/", $_POST['email']) == 0) { 
     // Выдаем куку на день с флажком об ошибке в поле email.
     setcookie('email_error', '1', time() + 24 * 60 * 60);
@@ -92,6 +130,7 @@ else {
     // Сохраняем ранее введенное в форму значение на год.
     setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60 * 365);
   }
+
   if (empty($_POST['biography']) || preg_match('/^[a-zA-Z.,!-]+$/' , $_POST['biography']) == 0) {  
     // Выдаем куку на день с флажком об ошибке в поле biography.
     setcookie('biography_error', '1', time() + 24 * 60 * 60);
@@ -101,7 +140,18 @@ else {
     // Сохраняем ранее введенное в форму значение на год.
     setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60 * 365);
   }
-  if (empty($_POST['yob']) || preg_match('/^[0-9]+$/' , $_POST['yob']) == 0) {  
+
+  if (!isset($_POST['radio-pol'])) {  
+    // Выдаем куку на день с флажком об ошибке в поле biography.
+    setcookie('radio-pol_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    // Сохраняем ранее введенное в форму значение на год.
+    setcookie('radio-pol_value', $_POST['radio-pol'], time() + 30 * 24 * 60 * 60 * 365);
+  }
+
+  if (empty($_POST['yob']) || preg_match('/^[0-9]+$/' , $_POST['yob']) == 0 || ($_POST['yob']>2021 || $_POST['yob']<1900)) {  
     // Выдаем куку на день с флажком об ошибке в поле biography.
     setcookie('yob_error', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
@@ -110,6 +160,33 @@ else {
     // Сохраняем ранее введенное в форму значение на год.
     setcookie('yob_value', $_POST['yob'], time() + 30 * 24 * 60 * 60 * 365);
   }
+
+  if (!isset($_POST['radio-kon'])) {  
+    // Выдаем куку на день с флажком об ошибке в поле biography.
+    setcookie('radio-kon_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    // Сохраняем ранее введенное в форму значение на год.
+    setcookie('radio-kon_value', $_POST['radio-kon'], time() + 30 * 24 * 60 * 60 * 365);
+  }
+  
+  if (!isset($_POST['sp-sp'])) {  
+    // Выдаем куку на день с флажком об ошибке в поле biography.
+    setcookie('ability_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    // Сохраняем ранее введенное в форму значение на год.
+    setcookie('ability_values', serialize($_POST['sp-sp']), time() + 30 * 24 * 60 * 60 * 365);
+  }
+
+  if (!isset($_POST['contract'])) {  
+    // Выдаем куку на день с флажком об ошибке в поле biography.
+    setcookie('contract_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  
 
 
 
@@ -124,41 +201,57 @@ else {
     setcookie('email_error', '', 100000);
     setcookie('biography_error', '', 100000);
     setcookie('yob_error', '', 100000);
+    setcookie('radio-pol_error', '', 100000);
+    setcookie('radio-kon_error', '', 100000);
+    setcookie('ability_error', '', 100000);
+    setcookie('contract_error', '', 100000);
 
-    // Сохранение в базу данных.
-    $user = 'u24095';
-    $pass = '8452445';
-    $db = new PDO('mysql:host=localhost;dbname=u24095', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
-// Подготовленный запрос. Не именованные метки.
-
-    try {
-      $str = implode(',',$_POST['sp-sp']);
-      
-      $stmt = $db->prepare("INSERT INTO appl SET fio = ?, email = ?, yob = ?, pol = ?, limb = ?, biograghy = ?");
-      $stmt -> execute([$_POST['fio'],$_POST['email'],$_POST['yob'],$_POST['radio-pol'],$_POST['radio-kon'],$_POST['biography']]);
-
-      $stmt = $db->prepare("INSERT INTO abilities SET abilities = ?");
-      $stmt -> execute([$str]);
-      
-    }
-    catch(PDOException $e){
-      print('Error : ' . $e->getMessage());
-      exit();
-    }
-    // TODO: тут необходимо удалить остальные Cookies.
+    
   }
-
-  // Сохранение в XML-документ.
-  // ...
-
-  // Сохраняем куку с признаком успешного сохранения.
-  setcookie('save', '1');
-
-  // Делаем перенаправление.
-  header('Location: index.php');
-}
 
 // Сохранение в базу данных.
 
+$user = 'u24095';
+$pass = '8452445';
+$db = new PDO('mysql:host=localhost;dbname=u24095', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
+// Подготовленный запрос. Не именованные метки.
+
+try {
+  $str = implode(',',$_POST['sp-sp']);
+  
+  $stmt = $db->prepare("INSERT INTO appl SET fio = ?, email = ?, yob = ?, pol = ?, limb = ?, biograghy = ?");
+  $stmt -> execute([$_POST['fio'],$_POST['email'],$_POST['yob'],$_POST['radio-pol'],$_POST['radio-kon'],$_POST['biography']]);
+
+  $stmt = $db->prepare("INSERT INTO abilities SET abilities = ?");
+  $stmt -> execute([$str]);
+  
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+//  stmt - это "дескриптор состояния".
+ 
+//  Именованные метки.
+//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
+//$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
+ 
+//Еще вариант
+/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
+*/
+
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
+header('Location: ?save=1');
+}
